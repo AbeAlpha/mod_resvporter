54e7d10a5c11ef048b3692ad993bdabb617d51a0
AbeAlpha <spamersdemierda@gmail.com>
2015-02-01 09:52:21 +0100

    Fix License

--- mod_resvporter.php
+++ mod_resvporter.php
@@ -1,4 +1,11 @@
 <?php
+/*------------------------------------------------------------------------
+# mod_resvporter - Responsive Viewporter
+# ------------------------------------------------------------------------
+# author    AbeAlpha
+# @license  http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: https://github.com/AbeAlpha/mod_resvporter
+-------------------------------------------------------------------------*/
 defined('_JEXEC') or die;
 require( JModuleHelper::getLayoutPath('mod_resvporter'));
 ?>
